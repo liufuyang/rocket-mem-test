a3de18b48152d907723624493580ba52ddd125fc
Fuyang Liu <liufuyang@users.noreply.github.com>
2018-10-10 22:03:13 +0200

    Further simplify

--- src/main.rs
+++ src/main.rs
@@ -3,42 +3,14 @@
 
 extern crate rocket;
 extern crate rocket_contrib;
-#[macro_use]
-extern crate serde_derive;
-extern crate uuid;
-
-use rocket_contrib::Json;
-use std::collections::HashMap;
-use uuid::Uuid;
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-struct User {
-    token: String,
-}
 
 #[get("/")]
 fn index() -> String {
     "Good".to_owned()
 }
 
-#[get("/login")]
-fn login() -> Json<HashMap<String, User>> {
-    let mut map = HashMap::new();
-
-    let uuid = Uuid::new_v4();
-
-    map.insert(
-        "1".to_owned(),
-        User {
-            token: uuid.to_string(),
-        },
-    );
-
-    Json(map.clone())
-}
-
 fn rocket() -> rocket::Rocket {
-    rocket::ignite().mount("/", routes![index, login])
+    rocket::ignite().mount("/", routes![index])
 }
 
 fn main() {
